00815f105c2c0ebaa8f0a388215cf6d1c4b665a1
Gerasimova Anastasia <ger.an.a@yandex.ru>
2022-11-07 21:52:10 +0300

    fix: исправить согласно ревью

--- utils/errorSelector.js
+++ utils/errorSelector.js
@@ -1,9 +1,11 @@
-const RegisterError = require('../errors/AccessError');
+const RegisterError = require('../errors/RegisterError');
 const BadRequestError = require('../errors/BadRequestError');
 const DefaultError = require('../errors/DefaultError');
 
-module.exports.errorSelector = (err) => {
+function errorSelector(err) {
   if (err.code === 11000) return new RegisterError();
   if (err.name === 'ValidationError' || err.name === 'CastError') return new BadRequestError();
   return new DefaultError();
-};
+}
+
+module.exports = errorSelector;
